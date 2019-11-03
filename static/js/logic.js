d3c08f89d1deda20c12cb4990209a9c3d9b21937
Varenka Rico <45055499+VarenkaRico@users.noreply.github.com>
2019-11-03 00:12:22 -0600

    Add files via upload

---
+++ static/js/logic.js
@@ -0,0 +1,97 @@
+//DATA SET
+var queryURL="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+var faultlinesURL="https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
+
+//Query the URL
+d3.json(queryURL,function(data){
+    createFeatures(data.features); //Call createFeatures Function
+});
+
+//EARTHQUAKE PopUp information
+//const EarthquakesMarkers= []
+function createFeatures (earthquakeData){
+    function onEachFeature(feature,layer){
+        layer.bindPopup("<h3>"+feature.properties.place + "</h3>"+ //Popup with Place info
+        "<hr><p>" + new Date(feature.properties.time)+"</p>"+ //Popup with Date info
+        "<p>" + "Magnitude: "+feature.properties.mag + "</p>") //Popup with Magnitude info
+    }    
+        
+//geoJSON layer
+    var earthquakes=L.geoJSON(earthquakeData,{
+
+        pointToLayer: function(earthquakeData, latlng) {
+            return L.circle(latlng, {
+              radius: earthquakeData.properties.mag*70000,
+              color: color(earthquakeData.properties.mag),
+              fillOpacity: 0.5,
+              stroke: false
+            });
+        },
+        onEachFeature:onEachFeature
+    });
+    createMap(earthquakes); //Call createMap Function
+
+}
+
+function color(magnitude){
+    if (magnitude < 2){
+        return "green"
+    }
+    else if (magnitude <4){
+        return "yellow"
+    }
+    else{
+        return "red"
+    }
+}
+
+var EarthquakeLayer = L.layerGroup(earthquakes);
+
+//BASE MAP
+
+function createMap(earthquakes){
+    var StreetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken: API_KEY
+    });
+
+    var SatelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
+
+    var DarkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+    });
+
+    var BaseMaps ={
+        "Street":StreetMap,
+        "Satellite":SatelliteMap,
+        "Dark":DarkMap
+    };
+
+    var OverlayMaps = {
+        "Earthquakes" : earthquakes,
+    };
+
+
+//ALL MAPS
+
+    var myMap = L.map("map",{
+    center:[0,0], // Map centered
+    zoom:2, //See full world
+    layers:[StreetMap,DarkMap,SatelliteMap,earthquakes] 
+    });
+
+    L.control.layers(BaseMaps, OverlayMaps,{
+        collapsed: false
+    }).addTo(myMap);
+}
+
